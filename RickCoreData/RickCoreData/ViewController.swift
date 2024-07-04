173f22d84f4c885fb0f3edecce53117651411ac3
Raheemspb <gabibovda@gmail.com>
2024-07-04 15:51:59 +0300

    First initial empty project and import CoreData

---
+++ RickCoreData/RickCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickCoreData
+//
+//  Created by Рахим Габибли on 04.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
